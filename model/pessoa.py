23d0c960e1879d448ed3a13d85b892f01eab32fe
gorgonun <polariusvulcano@gmail.com>
2021-03-09 21:41:50 -0300

    abstract person

--- model/pessoa.py
+++ model/pessoa.py
@@ -1,5 +1,24 @@
-class Pessoa():
+from abc import ABC, abstractmethod
 
-    def __init__(self, nome, cpf):
-        self.__nome = nome
-        self.__cpf = cpf
+
+class Pessoa(ABC):
+
+    @property
+    @abstractmethod
+    def nome(self):
+        pass
+
+    @property
+    @abstractmethod
+    def cpf(self):
+        pass
+
+    @nome.setter
+    @abstractmethod
+    def nome(self, nome):
+        pass
+
+    @cpf.setter
+    @abstractmethod
+    def cpf(self, cpf):
+        pass
