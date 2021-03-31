5fb43e036818227623355fc35723625b79e3f86e
gorgonun <polariusvulcano@gmail.com>
2021-03-30 21:08:34 -0300

    verifications, load data, cnpj

--- view/viewCadastroVendedor.py
+++ view/viewCadastroVendedor.py
@@ -4,41 +4,50 @@ from .view import View
 
 class ViewCadastroVendedor(View):
 
-    def rodar(self, window):
+    def rodar(self, window, verificacoes):
         while True:
             event, values = window.read()
-            valid = True
             if event == "Enviar":
+                window.Element("invalid_field").Update(visible=False)
+                window.Element("pass_not_equal").Update(visible=False)
+                window.Element("empty_field").Update(visible=False)
+
                 if self.tem_valores_vazios(values):
                     window.Element("empty_field").Update(visible=True)
+                    continue
 
-                elif values["password"] != values["confirm_pass"]:
+                if values["senha"] != values["confirm_pass"]:
                     window.Element("pass_not_equal").Update(visible=True)
+                    continue
 
-                else:
-                    values.pop("confirm_pass")
-                    return self.voltar(result=values)
+                campo_invalido = self.identificar_campos_invalidos(verificacoes, values)
+                if campo_invalido:
+                    window.Element("invalid_field").Update(f"{campo_invalido[0]} invalido: {campo_invalido[1]}", visible=True)
+                    continue
+
+                values.pop("confirm_pass")
+                return self.voltar(result=values)
                 
             if event == "Voltar ao menu" or event == sg.WIN_CLOSED:
                 return self.voltar()
 
-    def comecar(self):
+    def comecar(self, verificacoes=None):
         layout = [
             [sg.Text("Cadastro de vendedor")],
-            [sg.Text("Preencha todos os campos", key="empty_field")],
+            [sg.Text("", size=(50, 2), key="invalid_field", visible=False)],
+            [sg.Text("Preencha todos os campos", key="empty_field", visible=False)],
             [sg.Text("Nome", size=(20, 1)), sg.Input(key="nome")],
             [sg.Text("CPF", size=(20, 1)), sg.Input(key="cpf")],
-            [sg.Text("Conta Bancaria", size=(20, 1)), sg.Input(key="acc")],
-            [sg.Text("Senhas nao batem", key="pass_not_equal")],
-            [sg.Text("Senha", size=(20, 1)), sg.Input(key="password", password_char="*")],
+            [sg.Text("Conta Bancaria", size=(20, 1)), sg.Input(key="conta_bancaria")],
+            [sg.Text("CNPJ", size=(20, 1)), sg.Input(key="cnpj")],
+            [sg.Text("Senhas nao batem", key="pass_not_equal", visible=False)],
+            [sg.Text("Senha", size=(20, 1)), sg.Input(key="senha", password_char="*")],
             [sg.Text("Confirmar Senha", size=(20, 1)), sg.Input(key="confirm_pass", password_char="*")],
             [sg.Submit("Enviar")],
             [sg.Button("Voltar ao menu")]
         ]
 
         window = sg.Window("Cadastro de vendedor", layout=layout, element_justification='c').Finalize()
-        window.Element("pass_not_equal").Update(visible=False)
-        window.Element("empty_field").Update(visible=False)
-        result = self.rodar(window)
+        result = self.rodar(window, verificacoes or {})
         window.close()
         return result
