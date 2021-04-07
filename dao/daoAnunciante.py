3d6d9343932015136aae3d1fb42c3998b18d3e91
Pedro Nobrega <pedro.dias.nobrega@gmail.com>
2021-04-06 23:06:34 -0300

    verificação de cpf já existente

--- dao/daoAnunciante.py
+++ dao/daoAnunciante.py
@@ -5,6 +5,7 @@ from model.anunciante import Anunciante
 class DaoAnunciante(DaoAbstrato):
     def __init__(self, data_source):
         self.__data_source = data_source
+        self.__cache = None
 
     @property
     def data_source(self):
@@ -21,10 +22,11 @@ class DaoAnunciante(DaoAbstrato):
             for (k, v) in dados:
                 linha += f"{v},"
             src.write(linha[:-1] + "\n")
+        self.__cache = self.carregar_dados(Anunciante, self.__data_source)
 
     def existe_cpf(self, cpf):
         if not self.__cache:
-            self.carregar_dados()
+            self.__cache = self.carregar_dados(Anunciante, self.__data_source)
         for vendedor in self.__cache:
             if vendedor.cpf == cpf:
                 return True
